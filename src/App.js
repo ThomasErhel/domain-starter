1dcc8cd3d3ed0d76b031fe61d6eeffe42b88ebe8
Thomas Erhel <thomas.erhel@gmail.com>
2022-07-28 10:28:19 +0200

    Ship domain store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,167 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
+import {ethers} from "ethers";
+import Domains from './utils/Domains.json';
 
-// Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const tld = '.octocat';
+const CONTRACT_ADDRESS = '0x9071bd6D41B2E18D4019d12c74F1E246e51F2EFF';
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState('');
+  const [domain, setDomain] = useState('');
+  const [record, setRecord] = useState('');
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask -> https://metamask.io/");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log('Make sure you have metamask!');
+      return;
+    } else {
+      console.log('We have the ethereum object', ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log('Found an authorized account:', account);
+      setCurrentAccount(account);
+    } else {
+      console.log('No authorized account found');
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <div className="connect-wallet-container">
+	  <a href="https://github.com/octocat/">
+      <img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="Octocat" width="512" />
+	  </a>
+      <button onClick={connectWallet} className="cta-button connect-wallet-button">
+        Connect Wallet
+      </button>
+    </div>
+    );
+
+	const renderInputForm = () =>{
+		return (
+			<div className="form-container">
+				<div className="first-row">
+					<input
+						type="text"
+						value={domain}
+						placeholder='domain'
+						onChange={e => setDomain(e.target.value)}
+					/>
+					<p className='tld'> {tld} </p>
+				</div>
+
+				<input
+					type="text"
+					value={record}
+					placeholder='whats ur octocat power'
+					onChange={e => setRecord(e.target.value)}
+				/>
+
+				<div className="button-container">
+					<button className='cta-button mint-button' onClick={mintDomain}>
+						Mint
+					</button>  
+					<button className='cta-button mint-button' disabled={null} onClick={null}>
+						Set data
+					</button>  
+				</div>
+
+			</div>
+		);
+	}
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+  const mintDomain = async () => {
+	// Don't run if the domain is empty
+	if (!domain) { return }
+	// Alert the user if the domain is too short
+	if (domain.length < 3) {
+		alert('Domain must be at least 3 characters long');
+		return;
+	}
+	// Calculate price based on length of domain (change this to match your contract)	
+	// 3 chars = 0.5 MATIC, 4 chars = 0.3 MATIC, 5 or more = 0.1 MATIC
+	const price = domain.length === 3 ? '0.5' : domain.length === 4 ? '0.3' : '0.1';
+	console.log("Minting domain", domain, "with price", price);
+  try {
+    const { ethereum } = window;
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, Domains.abi, signer);
+
+			console.log("Going to pop wallet now to pay gas...")
+      let tx = await contract.register(domain, {value: ethers.utils.parseEther(price)});
+      // Wait for the transaction to be mined
+			const receipt = await tx.wait();
+
+			// Check if the transaction was successfully completed
+			if (receipt.status === 1) {
+				console.log("Domain minted! https://mumbai.polygonscan.com/tx/"+tx.hash);
+				
+				// Set the record for the domain
+				tx = await contract.setRecord(domain, record);
+				await tx.wait();
+
+				console.log("Record set! https://mumbai.polygonscan.com/tx/"+tx.hash);
+				
+				setRecord('');
+				setDomain('');
+			}
+			else {
+				alert("Transaction failed! Please try again");
+			}
+    }
+  }
+  catch(error){
+    console.log(error);
+  }
+}
 
   return (
-		<div className="App">
-			<div className="container">
-
-				<div className="header-container">
-					<header>
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <header>
             <div className="left">
-              <p className="title">🐱‍👤 Ninja Name Service</p>
+              <p className="title">🐱‍💻 Octocat Name Service</p>
               <p className="subtitle">Your immortal API on the blockchain!</p>
             </div>
-					</header>
-				</div>
+          </header>
+        </div>
+
+		{!currentAccount && renderNotConnectedContainer()}
+		{currentAccount && renderInputForm()}
 
         <div className="footer-container">
 					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
